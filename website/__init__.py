7e073a40f1f5e79bf75dd37261376a10d75449d4
Krakazyabra21 <krakazyabra2102@mail.ru>
2023-11-17 03:46:59 +0300

    almost done

--- website/__init__.py
+++ website/__init__.py
@@ -1,16 +1,29 @@
 from flask import Flask
+from os import path
 from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+DB_NAME = "bd_of_bigpapa.db"
 
 def create_app():
     app = Flask(__name__)
-    # db = SQLAlchemy()
-    # DB_NAME = "bd_of_bigpapa.bd"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
-    # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    # db.init_app(app)
 
     from .views import views
     app.register_blueprint(views, url_prefix="/")
 
+    from .bd_commands import hrs
+
+    with app.app_context():
+        db.create_all()
+    # create_database(app)
+
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app)
+        print('Created Database!')
--- website/views.py
+++ website/views.py
@@ -1,47 +1,40 @@
 from flask import Blueprint, render_template, request, jsonify, redirect, url_for
-# from main import db
-
+from . import db
 views = Blueprint('views', __name__)
 
-@views.route("/", methods= ['GET', 'POST'])
+@views.route("/", methods= ['GET','POST'])
 def home():
     if request.method == 'POST':
         data = request.form
         name = data.get('name')
         phone = data.get('phone')
         email = data.get('email')
+        print(name, phone, email)
 
-        # from bd_commands import HRs
-        # new_HR = HRs(name= name, phone= phone, email=email)
-        # db.session.add(new_HR)
-        # db.session.commit()
+        from website.bd_commands import hrs
+        new_HR = hrs(name= name, phone= phone, email=email)
+        print(new_HR)
+        db.session.add(new_HR)
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            print("GG")
+        print("New HR!")
 
-        # return redirect(url_for('/'))
-        return render_template("index.html")
+        result = db.session.query(hrs.id, hrs.name, hrs.phone, hrs.email).all()
+        print(result)
+
+
+
+
+        return redirect(url_for('views.home'))
+        # return render_template("index.html")
 
     return render_template("index.html")
 
-
-@views.route("/profile")
-def profile():
-    args = request.args
-    name = args.get('name')
-    return render_template("index.html", name=name)
-
-
-@views.route("/json")
-def get_json():
-    return jsonify({'name': 'xdd', 'coolness': 10})
-
-
-@views.route("/data")
-def get_data():
-    data = request.json
-    return jsonify(data)
-
-
-@views.route("/go-to-home")
-def go_to_home():
-    return redirect(url_for("views.get_json"))
-
-
+@views.route("/json", methods= ['GET'])
+def json():
+    from website.bd_commands import hrs
+    result = db.session.query(hrs.id, hrs.name, hrs.phone, hrs.email).all()
+    return f"<h1>{result}<h1>"
