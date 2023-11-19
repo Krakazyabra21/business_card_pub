5d17662d200fb86eb66d777365f2a18a510a5e7a
bismarckx <klimentmaslennikov@gmail.com>
2023-11-19 15:37:22 +0300

    bugfix: added adaptive design; added form validation

--- website/static/index.js
+++ website/static/index.js
@@ -1,2 +1,22 @@
-console.log("I am running");
-console.info("АБОБА")
+const form = document.querySelector(".form");
+const name = form.querySelector("#name");
+const phone = form.querySelector("#phone");
+const email = form.querySelector("#email");
+const button = form.querySelector(".form-group__button");
+
+const validation = (item) => {
+  item.addEventListener("input", () => {
+    const items = [item, form, email, button, phone];
+
+    if (item.validity.valid) {
+      items.forEach((formItem) => formItem.classList.remove("not-validated"));
+    } else {
+      form.classList.add("not-validated");
+      item.classList.add("not-validated");
+    }
+  });
+};
+
+validation(name);
+validation(phone);
+validation(email);
